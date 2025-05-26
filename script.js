6a4343c6dc97a12a83f70134f1c936406c0bf6ce
tanji007 <yash.agrwal404@gmail.com>
2025-05-26 23:24:06 +0530

    Created Etch-A-Sketch

---
+++ script.js
@@ -0,0 +1,103 @@
+// Declerations
+const container = document.querySelector(".container");
+let n = 16
+let size = `${500 / n}px`
+let color = "red"
+let boxes
+let isMouseDown = false;
+let isRainbow = false;
+
+//Grid Function
+
+function setupGrid() {
+    for (let i = 1; i <= n; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.append(row);
+
+        for (let j = 1; j <= n; j++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            box.style.height = size
+            box.style.width = size
+            row.append(box);
+        }
+    }
+    boxes = document.querySelectorAll(".box");
+
+    document.body.addEventListener("mousedown", () => {
+        isMouseDown = true;
+    });
+    document.body.addEventListener("mouseup", () => {
+        isMouseDown = false;
+    });
+
+    boxes.forEach((box) => {
+        box.addEventListener("mousedown", () => {
+            applyColor(box)
+        });
+
+        box.addEventListener("mouseover", () => {
+            if (isMouseDown) {
+            applyColor(box)
+            }
+        });
+    });
+}
+
+
+// Creating The Default grid
+
+setupGrid();
+
+
+// Rainbow Button
+
+let rainbow = document.querySelector(".rainbow");
+rainbow.addEventListener("click", () => {
+    isRainbow = true
+})
+
+// Rainbow Function
+
+function rainbowColor() {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+}
+
+// Applying color to box
+
+function applyColor(box) {
+    box.style.backgroundColor = isRainbow ? rainbowColor() : "black";
+}
+
+// Reset Functiom
+
+let reset = document.querySelector(".reset");
+reset.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        box.style.backgroundColor = "rgb(245, 245, 245)"
+    })
+    isRainbow = false
+})
+
+
+// Custom Grid
+
+let grid = document.querySelector(".grid");
+grid.addEventListener("click", () => {
+    document.querySelectorAll(".row").forEach(el => el.remove());
+    n = gridSize()
+    size = `${500 / n}px`
+    setupGrid();
+
+})
+
+// Taking grid size from user
+
+function gridSize() {
+    let inputSize;
+    while (inputSize === null || !(inputSize > 0 && inputSize <= 100)) {
+        inputSize = prompt("Enter Grid Size");
+    }
+    return inputSize
+}
